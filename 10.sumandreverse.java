a4712c0a94228c978dca8bf4e71b2e4678d627c4
sreesanjay s <104411099+Sreesanjay@users.noreply.github.com>
2023-01-07 18:27:07 +0530

    question 10 sum and reverse of a number

---
+++ 10.sumandreverse.java
@@ -0,0 +1,32 @@
+//question 10
+
+
+import java.util.Scanner;
+
+
+class Num {
+    int number, sum = 0;
+    StringBuffer reverse = new StringBuffer("");
+
+    Num(int number) {
+        this.number = number;
+        if (number == 0)
+            this.reverse.append("0");
+        while (number != 0) {
+            this.sum += number % 10;
+            this.reverse.append(number % 10);
+            number /= 10;
+        }
+    }
+}
+
+public class Number {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        Num n = new Num(s.nextInt());
+        System.out.println("Sum: " + n.sum);
+        System.out.println("Reverse: " + n.reverse);
+        s.close();
+    }
+}
